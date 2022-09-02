da155f0abcf17beb34783bc5d889e2a23c93c70f
paulo <afonsopaulo5777@gmail.com>
2022-09-02 11:26:19 -0300

    Primeiro comiit do site da igreja

---
+++ inserçao/insercao.js
@@ -0,0 +1,65 @@
+//aqui vem as funcoes em blocos pra editar o html
+
+function doc(a,b){
+    document.getElementById(a).innerText = b
+}
+
+
+function fraseNoticia(a,b) {
+    doc("frseNoticia",a)
+    doc("autor",b)
+}
+
+function proximoCulto(a,b,c,d,e,f,g) {
+    doc("p1",a)
+    doc("p2",b)
+    doc("p3",c)
+    doc("p4",d)
+    doc("p5",e)
+    doc("p6",f)
+    doc("p7",g)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//aqui vai ficar a invocaçoes da funcoes
+
+fraseNoticia("Os mais poderosos intelectos da Terra não podem compreender a Deus. Os homens podem estar sempre a pesquisar, sempre a aprender, e ainda há, para além, o infinito.",
+
+"Ellen G. White")
+
+proximoCulto("Ministerio Pessoal","Enos","Irma Leia","Luzidete","Karol","Santa","Luana")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
